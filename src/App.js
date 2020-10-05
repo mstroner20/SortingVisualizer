bf07e43c4aac8d11aafa1d6621b37edb48bf779b
Michael Stroner <Michaelstroner100@gmail.com>
2020-10-05 01:14:05 -0500

    First react test
    Simple tests done to understand React.
    Non important to project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
+//This file holds all logic/ HTMl / Styling in one file 
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Tweet from './Tweet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+
+  
+
+  const counter = 0; 
+
+
+
+  return(
+    <div className = "app"> 
+      <h1>This is the app component</h1>
+      <h1>Hello React</h1>
+      <Tweet />
+
+      
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Tweet.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css'
+
+function Tweet(){
+    return (
+
+        <div className = "tweet">
+            <h3>Name</h3>
+            <p>This is a random Tweet</p>
+            <h3>Number of likes</h3>
+
+        </div>
+       
+        
+    );
+}
+
+
+export default Tweet; 
